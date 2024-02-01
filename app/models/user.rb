39f957c1a0e9aaa576b441aae86b279cde4ec144
kurumakazuto <153343673+kurumakazuto@users.noreply.github.com>
2024-02-01 12:23:45 +0000

    [Finish]応用課題３

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
   validates :name, length:{minimum:2,maximum:20},uniqueness: true
-  validates :introduction, length:{maximum:50} 
+  validates :introduction, length:{maximum:50}
 
   has_one_attached :profile_image
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index,:show, :edit, :create, :update, :destroy]
-  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as:'about'
+  resources :users, only: [:index,:show, :edit, :create, :update, :destroy]
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
