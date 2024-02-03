a0e7162aa523e808d32abd5e22e297febdf04ee9
kurumakazuto <153343673+kurumakazuto@users.noreply.github.com>
2024-02-03 06:42:03 +0000

    [Add]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,5 +1,5 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
@@ -7,17 +7,16 @@ class BookCommentsController < ApplicationController
     comment.save
     redirect_to book_path(book)
   end
-  
+
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.find_by(book_comment_params)
-    comment.book_id = book.id
-    comment.destroy
+    BookComment.find(params[:id]).destroy
     redirect_to request.referer
   end
-  
+
+  private
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
 end
