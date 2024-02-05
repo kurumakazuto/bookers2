107ebae3f739de295b3508c4d349f4ab2532524d
kurumakazuto <153343673+kurumakazuto@users.noreply.github.com>
2024-02-05 07:51:21 +0000

    [Finish]応用課題５

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,10 +4,26 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-def favorited_by?(user)
-  favorites.exists?(user_id: user.id)
-end
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
   validates :title, presence: true
   validates :body, presence: true, length:{maximum:200}
+
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = Book.where("title LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = Book.where("title LIKE?","%#{word}%")
+    else
+      @user = Book.all
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  
+
 
   validates :name, length:{minimum:2,maximum:20},uniqueness: true
   validates :introduction, length:{maximum:50}
@@ -26,17 +26,31 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def follow(user)
     active_relationships.create(followed_id: user.id)
   end
-  
+
   def unfollow(user)
     active_relationships.find_by(followed_id: user.id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
-  
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
 end
