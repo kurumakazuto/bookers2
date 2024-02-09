5790f5c9eb4e43daf634e5a9debfeea9c5b0d536
kurumakazuto <153343673+kurumakazuto@users.noreply.github.com>
2024-02-09 13:54:34 +0000

    [Add]応用課題７

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+  
+  def create
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      @message = Message.new(message_params)
+      @message.user_id = current_user.id
+      @message.save
+    else
+      flash[:aler] = "メッセージ送信に失敗しました。"
+    end
+    render :validator unless @message.save
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:user_id, :room_id, :content)
+  end
+    
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :rooms, through: :entries
 
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
